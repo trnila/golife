a2434e20c71d77caa39900b8781696293030735f
Daniel Trnka <daniel.trnka@gmail.com>
2017-10-27 11:22:57 +0200

    using tcell for terminal gui

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
+	_ "time"
+	"math"
+	"github.com/gdamore/tcell"
+	"os"
+	"math/rand"
 )
 
 const DEAD = 0;
 const ALIVE = 1;
+
+type Viewport struct {
+	rows int
+	cols int
+	centerRow int
+	centerCol int
+	zoom int
+	screen tcell.Screen
+}
 
 type Cell int
 
@@ -76,38 +89,45 @@ func (b Board) AliveNeighbours(row, col int) int {
 	return count - int(b.Get(row, col))
 }
 
-func (b Board) Print() {
-	for r := 0; r < b.rows; r++ {
-		for c := 0; c < b.cols; c++ {
+func (b Board) Print(view *Viewport) {
+	var rowFrom = int(math.Max(0, float64(view.centerRow - view.rows / 2)))
+	var colFrom = int(math.Max(0, float64(view.centerCol - view.cols / 2)))
+	var rowTo = int(math.Min(float64(view.centerRow + view.rows / 2), float64(b.rows)))
+	var colTo = int(math.Min(float64(view.centerCol + view.cols / 2), float64(b.cols)))
+	for r := rowFrom; r < rowTo; r++ {
+		for c := colFrom; c < colTo; c++ {
 			if b.Get(r, c) == ALIVE {
-				fmt.Print("*")
+				view.screen.SetContent(c - colFrom, r - rowFrom, rune('*'), []rune(""), tcell.StyleDefault)
 			} else {
-				fmt.Print(" ")
+				view.screen.SetContent(c - colFrom, r - rowFrom, rune(' '), []rune(""), tcell.StyleDefault)
 			}
 		}
-		fmt.Print("\n")
 	}
+	view.screen.Show()
 }
 
 func calc(b *Board, start, to int) {
 	for r := start; r < to; r++ {
 		for c := 0; c < b.cols; c++ {
 			var aliveNeighbours = b.AliveNeighbours(r, c)
-			if b.Get(r, c) == ALIVE {
+			var state = b.Get(r, c)
+			if state == ALIVE {
 				if aliveNeighbours < 2 || aliveNeighbours > 3 {
-					b.Set(r, c, DEAD)
+					state = DEAD
 				} else {
-					b.Set(r, c, ALIVE)
+					state = ALIVE
 				}
 			} else if aliveNeighbours == 3 {
-				b.Set(r, c, ALIVE);
+				state = ALIVE
 			}
+
+			b.Set(r, c, state)
 		}
 	}
 }
 
 func main() {
-	const size = 10000;
+	const size = 1000;
 	const threads = 4;
 	var b = NewBoard(size)
 
@@ -122,8 +142,75 @@ func main() {
 	b.Init(2, 2, ALIVE)
 	b.Init(2, 3, ALIVE)
 
+	b.Init(20, 5, ALIVE)
+	b.Init(21, 5, ALIVE)
+	b.Init(22, 5, ALIVE)
+
+	b.Init(20, 170, ALIVE)
+	b.Init(21, 170, ALIVE)
+	b.Init(22, 170, ALIVE)
+
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+
+	cols, rows := s.Size()
+	var view = Viewport{
+		rows: rows,
+		cols: cols,
+		centerRow: rows / 2,
+		centerCol: cols / 2,
+	}
+	view.screen = s
+
+	for i := 0; i < 1000000; i++ {
+		b.Init(rand.Intn(size), rand.Intn(size), Cell(rand.Intn(2)))
+	}
+
+
+	quit := make(chan struct{})
+
+
+
+	go func() {
+		for {
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyCtrlC {
+					close(quit)
+					return
+				} else if ev.Key() == tcell.KeyRight {
+					view.centerCol += 1
+				} else if ev.Key() == tcell.KeyLeft {
+					view.centerCol -= 1
+				} else if ev.Key() == tcell.KeyUp {
+					view.centerRow -= 1
+				} else if ev.Key() == tcell.KeyDown {
+					view.centerRow += 1
+				}
+			}
+		}
+	}()
+
+
+	go compute(threads, size, b, &view)
+
+	<- quit
+	s.Fini()
+}
+
+
+func compute(threads int, size int, b *Board, view *Viewport) {
 	for {
-		start := time.Now()
+		//start := time.Now()
 
 		var wait sync.WaitGroup
 		wait.Add(threads)
@@ -133,7 +220,7 @@ func main() {
 				defer wait.Done()
 
 				var from = (size + threads) / threads * t;
-				var to = from + (size + threads) / threads
+				var to = from + (size+threads)/threads
 				if to > size {
 					to = size
 				}
@@ -145,8 +232,9 @@ func main() {
 		wait.Wait()
 
 		b.generation++
+		b.Print(view)
 
-		elapsed := time.Since(start)
-		fmt.Println(elapsed)
+		//elapsed := time.Since(start)
+		//fmt.Println(elapsed)
 	}
 }
